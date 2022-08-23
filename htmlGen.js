da8ca414be61eb77de993be1a9db43646b136037
Gavin <gavinporlich@gmail.com>
2022-08-23 00:40:32 -0500

    Added styling to website after being generated

--- htmlGen.js
+++ htmlGen.js
@@ -18,9 +18,17 @@ function makeHTMLPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
     </head>
     <body>
-        ${empDivArr}
+        <header>
+        <div class="jumbotron jumbotron-fluid text-center text-white bg-dark">
+            <div class="container">
+                <h1 class="display-4">Employee Roster</h1>
+            </div>
+        </div>
+        </header>
+        <div class="card-deck d-flex justify-content-center">${empDivArr}</div>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     </body>
@@ -32,40 +40,40 @@ function makeEmployeeCards() {
     team.forEach((emp) => {
         if (emp.type === "Manager") {
             empDivArr+=`
-            <div class="card col-3">
+            <div class="card col-lg-2 col-md-12 text-center text-white bg-dark">
                 <div class="card-body">
                     <h5 class="card-title">Employee Name: ${emp.name}</h5>
                     <p class="card-body">Employee Role: ${emp.type}</p>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Employee ID: ${emp.id}</li>
-                        <li class="list-group-item">Employee Email: <a href="mailto:${emp.email}?subject=Mail from our Website">${emp.email}</a></li>
-                        <li class="list-group-item">Office Number: ${emp.officeNum}</li>
+                        <li class="list-group-item text-white bg-dark">Employee ID: ${emp.id}</li>
+                        <li class="list-group-item text-white bg-dark">Employee Email: <a href="${emp.email}">${emp.email}</a></li>
+                        <li class="list-group-item text-white bg-dark">Office Number: ${emp.officeNum}</li>
                     </ul>
                 </div>
             </div>`
         } else if (emp.type === "Engineer") {
             empDivArr+=`
-            <div class="card col-3">
+            <div class="card col-lg-2 col-md-12 text-center text-white bg-dark">
                 <div class="card-body">
                     <h5 class="card-title">Employee Name: ${emp.name}</h5>
                     <p class="card-body">Employee Role: ${emp.type}</p>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Employee ID: ${emp.id}</li>
-                        <li class="list-group-item">Employee Email: <a href="mailto:${emp.email}?subject=Mail from our Website">${emp.email}</a></li>
-                        <li class="list-group-item">Engingeer Github: <a href="https://github.com/${emp.github}">${emp.github}</a></li>
+                        <li class="list-group-item text-white bg-dark">Employee ID: ${emp.id}</li>
+                        <li class="list-group-item text-white bg-dark">Employee Email: <a href="mailto:${emp.email}">${emp.email}</a></li>
+                        <li class="list-group-item text-white bg-dark">Engingeer Github: <a href="https://github.com/${emp.github}" target="_blank">${emp.github}</a></li>
                     </ul>
                 </div>
             </div>`
         } else {
             empDivArr+=`
-            <div class="card col-3">
+            <div class="card col-lg-2 col-md-12 text-center text-white bg-dark">
                 <div class="card-body">
                     <h5 class="card-title">Employee Name: ${emp.name}</h5>
                     <p class="card-body">Employee Role: ${emp.type}</p>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Employee ID: ${emp.id}</li>
-                        <li class="list-group-item">Employee Email: <a href="${emp.email}?subject=Mail from our Website">${emp.email}</a></li>
-                        <li class="list-group-item">Intern's School: ${emp.school}</li>
+                        <li class="list-group-item text-white bg-dark">Employee ID: ${emp.id}</li>
+                        <li class="list-group-item text-white bg-dark">Employee Email: <a href="${emp.email}">${emp.email}</a></li>
+                        <li class="list-group-item text-white bg-dark">Intern's School: ${emp.school}</li>
                     </ul>
                 </div>
             </div>`
